a8d713d67eb59f0040bf63d5b268afae1b845cbc
AdaleeVorbeck <adavor0529@students.maquoketaschools.org>
2026-01-13 20:20:32 +0000

    ILL LIVE FOR THOSE WHO ARENT AROUND

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+let i = 0;
+let todos = ['Laundry', 'Dishes', 'Clean Desk', 'Sweep Floor'];
+
+function markAsDone(todos) {
+    todos.array.forEach(todos => i -= todos); {
+    todos[i] = "done - " + todos[i];
+    i++;
+    }
+    return todos;
+}
+
+document.getElementById('result').innerHTML = markAsDone(todos).join('<br>');
+
 
