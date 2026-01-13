a0a89f1ab6f11747585334f799cb66ad6cb00d2d
AdaleeVorbeck <adavor0529@students.maquoketaschools.org>
2026-01-13 20:27:24 +0000

    YOU HAVE TO SEE ANOTHER DAYYYYYYYY

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-let i = 0;
 let todos = ['Laundry', 'Dishes', 'Clean Desk', 'Sweep Floor'];
 
 function markAsDone(todos) {
-    todos.array.forEach(todos => i -= todos); {
-    todos[i] = "done - " + todos[i];
-    i++;
-    }
-    return todos;
+    const updated = [];
+    todos.forEach(todo => {
+        updated.push('done - ' + todo);
+    });
+    return updated;
 }
 
 document.getElementById('result').innerHTML = markAsDone(todos).join('<br>');
 
 
+//should create a function named markAsDone that takes an array of todos and returns a new array containing the modified todo items
+//should loop through the todos, using forEach, and prepend the string 'done - ' to each todo description
